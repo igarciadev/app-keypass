3f6f4dc4979f18c4fc648e95be3d117b2900a26f
igarciadev <isaacgsanchez@gmail.com>
2021-07-25 16:07:24 +0000

    Add tabs page

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+    {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+            {
+                path: '',
+                redirectTo: '/tabs/safe-tab',
+                pathMatch: 'full'
+            },
+            {
+                path: 'safe-tab',
+                loadChildren: () => import('../list-tab/list-tab.module').then(m => m.ListTabPageModule)
+            },
+            {
+                path: 'password-tab',
+                loadChildren: () => import('../password-tab/password-tab.module').then(m => m.PasswordTabPageModule)
+            },
+            {
+              path: 'password-tab/create',
+              loadChildren: () => import('../password-tab/password-tab.module').then(m => m.PasswordTabPageModule)
+            },
+            {
+              path: 'password-tab/create/:secret',
+              loadChildren: () => import('../password-tab/password-tab.module').then(m => m.PasswordTabPageModule)
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/tabs/safe-tab',
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+})
+export class TabsPageRoutingModule { }
---
+++ src/app/pages/tabs/tabs.page.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UrlService } from 'src/app/services/url.service';
+import text from 'src/assets/text/tabs.text.json';
+
+@Component({
+    selector: 'app-tabs',
+    templateUrl: 'tabs.page.html'
+})
+export class TabsPage {
+
+    currentUrl: string;
+    text: any;
+
+    constructor(
+        private router: Router,
+        private urlService: UrlService
+    ) {
+        this.currentUrl = null;
+        this.text = text;
+    }
+
+    ngOnInit() {
+    }
+
+    ionViewWillEnter() {
+        this.urlService.currentUrl$
+            .subscribe((currentUrl: string) => {
+                this.currentUrl = currentUrl;
+            });
+    }
+
+    navigateToListTab() {
+        this.router.navigateByUrl('tabs/safe-tab', { replaceUrl: true });
+    }
+
+    navigateToPasswordTab() {
+        this.router.navigateByUrl('tabs/password-tab', { replaceUrl: true });
+    }
+
+    get show() {
+        return this.currentUrl !== null &&
+            (this.currentUrl === '/' ||
+                this.currentUrl === '/tabs/safe-tab' ||
+                this.currentUrl === '/tabs/password-tab');
+    }
+}
