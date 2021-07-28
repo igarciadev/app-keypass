5a3490c5d6cc8c285249368e28554fcfd8f1f43a
igarciadev <isaacgsanchez@gmail.com>
2021-07-28 06:55:01 +0000

    Add action popover component

---
+++ src/app/shared/action-popover/action-popover.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+import { NavParams, PopoverController } from '@ionic/angular';
+
+import { PassConfig } from 'src/app/models/pass-config.model';
+import text from 'src/assets/text/action-popover.component.text.json'
+
+@Component({
+    selector: 'app-popover-view',
+    templateUrl: './action-popover.component.html'
+})
+export class ActionPopoverComponent implements OnInit {
+
+    page: string;
+    passConfig: PassConfig;
+    text: any;
+
+    constructor(
+        private popoverController: PopoverController,
+        private navParams: NavParams
+    ) { }
+
+    ngOnInit() {
+        this.page = this.navParams.data.page;
+        this.passConfig = this.navParams.data.passcConfig;
+        this.text = text;
+    }
+
+    dismissPopover(action: string, passConfig: PassConfig) {
+        this.popoverController.dismiss({
+            item: { action: action, passConfig: passConfig }
+        });
+    }
+
+    editConfigPassAction() {
+        this.dismissPopover('edit', this.passConfig);
+    }
+
+    favoriteConfigAction() {
+        this.dismissPopover('favorite', this.passConfig);
+    }
+
+    shareConfigPassAction() {
+        this.dismissPopover('share', this.passConfig);
+    }
+
+    deleteConfigPassAction() {
+        this.dismissPopover('delete', this.passConfig);
+    }
+}
